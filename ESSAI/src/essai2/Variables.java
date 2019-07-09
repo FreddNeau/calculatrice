ad7a3663ea87a2d80a3f3c42d1d3ac5ccfc9c5a4
FRUP74971 <FRUP74971@GFI510146.annuaire.groupe.local>
2019-07-09 17:40:23 +0200

    dsdsd

--- ESSAI/src/essai2/Variables.java
+++ ESSAI/src/essai2/Variables.java
@@ -3,11 +3,14 @@ package essai2;
 public class Variables {
 
 	public static void main(String[] args) {
+		//Déclaration de variables
 		double var1 = 5;
 		double var2 = 10;
 		
+		//Calcul du quotient de var1 et var2
 		double quotient = var1 / var2;
 		
+		//Affichage du résultat
 		System.out.println(quotient);
 
 	}
