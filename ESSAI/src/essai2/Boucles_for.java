facb6d63b0745f3cd6a994a9837c4206ba40d996
FRUP74971 <FRUP74971@GFI510146.annuaire.groupe.local>
2019-07-16 10:39:18 +0200

    Rajout classe boucle_for

---
+++ ESSAI/src/essai2/Boucles_for.java
@@ -0,0 +1,20 @@
+package essai2;
+
+public class Boucles_for {
+
+	public static void main(String[] args) {
+
+		/*
+		 * for (int i = 1; i <=100; i++) { System.out.println("Le double de " + i +
+		 * " est " + 2*i);
+		 */
+
+		// table de multiplication de 9
+		final int NOMBRE = 4;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("9 multiplié par " + i + " est : " + (NOMBRE * i));
+		}
+
+	}
+
+}
